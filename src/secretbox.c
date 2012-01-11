5e9483efb72f4c6e275621dcfe27bcb3bc3351cf
Watson Ladd <watsonbladd@gmail.com>
2012-01-11 15:46:18 -0600

    Added NaCl-like interface. Still slow

---
+++ src/secretbox.c
@@ -0,0 +1,27 @@
+#include "secretbox.h"
+#include "aes256gcm.h"
+#include <strings.h>
+void crypto_secretbox_aes256gcm (unsigned char *c, unsigned char *m,
+                                 unsigned long long mlen, unsigned char *n,
+                                 unsigned char *k){
+  unsigned char j0[16];
+  j0[0]='U';
+  j0[1]='o';
+  j0[2]='f';
+  j0[3]='C';
+  memcpy(j0+4, n);
+  aes256gcm(c, m, mlen, n, k);
+}
+
+void crypto_secretbox_open_aes256gcm(unsigned char *m, unsigned char *c,
+                                     unsigned long long clen,
+                                     unsigned char *n,
+                                     unsigned char *k){
+  unsigned char j0[16];
+  j0[0]='U';
+  j0[1]='o';
+  j0[2]='f';
+  j0[3]='C';
+  memcpy(j0+4, n);
+  return aes256gcmdecrypt(m,c, clen, n, k);
+}
---
+++ src/secretbox.h
@@ -0,0 +1,14 @@
+#define crypto_secretbox_aes256gcm_KEYBYTES 32
+#define crypto_secretbox_aes256gcm_NONCEBYTES 8
+#define crypto_secretbox_aes256gcm_ZEROBYTES 16
+#define crypto_secretbox_aes256gcm_BOXZEROBYTES 0
+extern void crypto_secretbox_aes256gcm(unsigned char *c, 
+                                       unsigned char *m, 
+                                       unsigned long long mlen, 
+                                       unsigned char *n, 
+                                       unsigned char *k);
+extern int crypto_secretbox_aes256gcm_open(unsigned char *m, 
+                                           unsigned char *c, 
+                                           unsigned char *clen, 
+                                           unsigned char *n,
+                                           unsigned char *k);
