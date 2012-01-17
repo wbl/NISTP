2e3a9348f70f871cac5b78a249895e4e92eb4560
Watson Ladd <watsonbladd@gmail.com>
2012-01-16 20:05:21 -0600

    additional tests added.

--- test/fep256test.c
+++ test/fep256test.c
@@ -6,18 +6,23 @@ void outputpoly (fep256 *t){
   for(int i=0; i<10; i++){
     printf("%lld*2^(24*%d)+", t->c[i], i);
   }
-  printf("%lld*2^240\n", t->c[10], 10);
+  printf("%lld*2^240", t->c[10], 10);
 }
 
 int main(){
-  //test fep256.h
-  fep256 a;
+  //test fep256.h by creating a script of equations that should hold
   fep256 b;
   fep256 c;
-  fep256setzero(&a);
+  fep256setzero(&c);
   fep256setone(&b);
-  fep256add(&c, &a,&b);
-  outputpoly(&c);
+  printf("p=2^256-2^224+2^192+2^96-1\n");
+  for(int i=1; i<1024; i++){
+    printf("print(Mod(%d, p)==Mod(", i);
+    fep256add(&c, &c,&b);
+    outputpoly(&c);
+    printf(",p))\n");
+  }
+  printf("quit()\n");
   exit(0);
 }
     
