3e8e0411372130a5202ba1d1b0e3ad1f821f1793
Watson Ladd <watsonbladd@gmail.com>
2012-01-17 21:40:50 -0600

    Even ripping off OpenSSL, code needs cleanup

---
+++ src/fep256.h
@@ -0,0 +1,14 @@
+typedef __uint128_t uint128_t;
+typedef struct {
+  uint128_t c[4];
+} fep256;
+extern void fep256add(fep256 *c, fep256 *a, fep256 *b);
+extern void fep256mul(fep256 *c, fep256 *a, fep256 *b);
+extern void fep256cmov (fep256 *r, fep256 *x, unsigned char b);
+extern void fep256setzero(fep256 *r);
+extern void fep256setone(fep256 *r);
+extern void fep256pack(unsigned char c[32], fep256 *r);
+extern void fep256unpack(fep256 *r, unsigned char c[32]);
+extern void fep256sub(fep256 *c, fep256 *a, fep256 *b);
+extern void fep256inv(fep256 *c, fep256 *a);
+extern char fep246iszero(fep256 *a);
