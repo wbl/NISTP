829db8cfbbffe4af7e9229cface589f37d272b6a
Watson Ladd <watsonbladd@gmail.com>
2012-01-17 21:21:23 -0600

    Time to steal^H borrow from OpenSSL.

--- Makefile.mk
+++ Makefile.mk
@@ -9,13 +9,12 @@ LINK = -ltomcrypt
 % : %.o
 	$(CC) $(IOPTS) $(LOPTS) $(LINK) $^ -o $@
 all: test libnistp.a libnistp.so
-test: garithtest ctrtest aestest aes256gcmtest gpacktest fep256test
+test: garithtest ctrtest aestest aes256gcmtest gpacktest
 aes256gcmtest: aes256gcmtest.o aes256gcm.o aes256gcmtom.o rijndael.o ctr.o ghash.o unload64.o load64.o garith.o verify.o
 ctrtest: ctrtest.o ctr.o rijndael.o
 aestest: aestest.o rijndael.o
 garithtest: garithtest.o garith.o
 gpacktest: gpacktest.o garith.o
-fep256test: fep256.o fep256test.o
 libnistp.a: ctr.o rijndael.o garith.o aes256gcm.o secretbox.o ghash.o unload64.o load64.o verify.o
 	ar -r $@ $^
 libnistp.so: ctr.o rijndael.o garith.o aes256gcm.o secretbox.o ghash.o unload64.o load64.o verify.o
--- src/fep256.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <assert.h>
-#include "fep256.h"
-/*arithmetic mod p=2^256-2^224+2^192+2^96-1*/
-/*We use 11 64 bit limbs, with values mostly capped at 2^24.
-This arrangement lets us multiply and accumulate safely with very small
-number arithmetic. It also makes the awkward placements a bit better, while
-still permitting us to use 64 bit arithemetic.*/
