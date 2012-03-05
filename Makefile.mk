3b974124cf84386dd972e686c9c0c9e17b452320
Watson Ladd <watsonbladd@gmail.com>
2012-03-04 22:24:17 -0600

    Put ECDSA in the product

--- Makefile.mk
+++ Makefile.mk
@@ -5,11 +5,11 @@ IOPTS = -I../src -I../test -I/opt/local/include
 LOPTS = -L/opt/local/lib
 LINK = -ltomcrypt
 %.o: %.c
-	$(CC) $(CCOPTS) $(IOPTS)  -c $? -o $@
+	$(CC) $(CCOPTS) $(IOPTS)  -c $^ -o $@
 % : %.o
 	$(CC) $(CCOPTS) $(IOPTS) $(LOPTS) $(LINK) $^ -o $@
 all: test libnistp.a
-test: garithtest ctrtest aestest aes256gcmtest gpacktest fep256test curvetest scalarmulttest boxtest scp256test hashtest
+test: garithtest ctrtest aestest aes256gcmtest gpacktest fep256test curvetest scalarmulttest boxtest scp256test hashtest ecdsatest curveknown
 aes256gcmtest: aes256gcmtest.o aes256gcm.o aes256gcmtom.o rijndael.o ctr.o ghash.o unload64.o load64.o garith.o verify.o
 ctrtest: ctrtest.o ctr.o rijndael.o
 aestest: aestest.o rijndael.o
@@ -21,7 +21,9 @@ scalarmulttest: scalarmult.o curve.o randombytes.o scalarmulttest.o fep256.o
 boxtest: boxtest.o box.o scalarmult.o curve.o secretbox.o ghash.o garith.o aes256gcm.o unload64.o load64.o verify.o rijndael.o ctr.o fep256.o randombytes.o
 scp256test: scp256.o scp256test.o randombytes.o
 hashtest: hash.o blocks.o hashtest.o
-libnistp.a: ctr.o rijndael.o garith.o aes256gcm.o secretbox.o ghash.o unload64.o load64.o verify.o fep256.o scalarmult.o curve.o box.o
+ecdsatest: ecdsa.o randombytes.o scp256.o curve.o fep256.o ecdsatest.o hash.o verify.o blocks.o
+curveknown: curveknown.o fep256.o curve.o scp256.o
+libnistp.a: ctr.o rijndael.o garith.o aes256gcm.o secretbox.o ghash.o unload64.o load64.o verify.o fep256.o scalarmult.o curve.o box.o scp256.o ecdsa.o
 	ar -r $@ $^
 .PHONY : all
 .PHONY : test
